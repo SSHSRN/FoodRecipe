f8d453e2007963373a09f8f3ff42c385e57e31cb
SRIHARI S <sshsrn@gmail.com>
2023-01-31 23:04:09 +0530

    feat: added dependancies
    Signed-off-by: SRIHARI S <sshsrn@gmail.com>

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const axios = require('axios');
 const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.telegram_bot_api_key);
+
+bot.start((ctx) => ctx.reply(`Welcome ${ctx.chat.first_name} \n\nI am a bot.`));
+
+bot.launch();
