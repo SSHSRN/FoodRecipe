1257f26a4a1b0cfce3ef456a94025a91401c1a5d
SRIHARI S <sshsrn@gmail.com>
2023-02-01 22:34:57 +0530

    feat: added API to get recipe
    Signed-off-by: SRIHARI S <sshsrn@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.telegram_bot_api_key);
+const apiURL = process.env.recipe_api_url;
+
+const getRandomRecipe = async () => {
+    // add http headers to the request
+    const headers = {
+        'x-api-key': process.env.recipe_api_key
+    };
+    // make the request
+    const response = await axios.get(apiURL, { headers });
+    return response.data;
+}
 
 bot.start((ctx) => ctx.reply(`Welcome ${ctx.chat.first_name} \n\nI am a bot.`));
+
+bot.command('random', async (ctx) => {
+    const recipe = await getRandomRecipe();
+    console.log(recipe);
+    console.log(recipe.recipes[0].image);
+    ctx.replyWithPhoto(recipe.recipes[0].image, {
+        caption: recipe.recipes[0].title
+    })
+});
 
 bot.launch();
